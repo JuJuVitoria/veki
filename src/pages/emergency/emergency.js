c21949ee2d1e011d4a29ff51ee85e70781e1c4a7
JujuVitoria <juliavitoriadaluz5@gmail.com>
2025-06-04 17:19:35 -0300

    feat(emergency): implementa tela sobreposta de pedidos.

--- src/pages/emergency/emergency.js
+++ src/pages/emergency/emergency.js
@@ -15,16 +15,16 @@ function criarCard(emergencia) {
     infoDiv.classList.add('card-info');
 
     const categoriaDiv = document.createElement('div');
-    categoriaDiv.classList.add('card-info__categoria');
+    categoriaDiv.classList.add('info__categoria');
     const textCategoria = document.createElement('p');
-    textCategoria.classList.add('card-info__p')
+    textCategoria.classList.add('info__p')
     textCategoria.innerText = emergencia.categoria;
     categoriaDiv.appendChild(textCategoria);
 
     const relevanciaDiv = document.createElement('div');
-    relevanciaDiv.classList.add('card-info__relevancia');
+    relevanciaDiv.classList.add('info__relevancia');
     const textRelevancia = document.createElement('p');
-    textRelevancia.classList.add('card-info__p');
+    textRelevancia.classList.add('info__p');
     textRelevancia.innerText = emergencia.relevancia;
     relevanciaDiv.appendChild(textRelevancia);
 
@@ -75,6 +75,15 @@ function criarCard(emergencia) {
     botao.classList.add('button-orange');
     botao.innerText = 'Ver mais sobre os pedidos';
 
+    botao.addEventListener("click", () => {
+        const telaPedidos = document.getElementById('telaPedidos');
+        if (telaPedidos.style.display === "flex") {
+            telaPedidos.style.display = "none";
+        } else {
+            telaPedidos.style.display = "block";
+        }
+    });
+
     // Monta tudo no card
     card.appendChild(infoDiv);
     card.appendChild(titulo);
@@ -95,3 +104,7 @@ emergencias.forEach((emergencia) => {
 const form = document.getElementById('formFiltro');
 
 form.addEventListener('submit', aplicarForm);
+
+document.getElementById("btnCloseTelaPedidos")?.addEventListener("click", () => {
+    document.getElementById("telaPedidos").style.display = "none";
+});
